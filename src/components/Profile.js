7bf80b39c630d0d8022888af1a268a8d0a1b7c59
ChihTeng Ma <chihtengma416@gmail.com>
2023-09-06 11:59:24 -0400

    nav completed

---
+++ src/components/Profile.js
@@ -0,0 +1,13 @@
+export const Profile = () => {
+   return (
+      <section id="profile">
+         <div className="section__pic-container">
+            <img src="" alt="ChihTeng Ma profile picture" />
+         </div>
+         <div className="section__text">
+            <p className="section__text__p1">Hello, I'm</p>
+            <h1 className="title">ChihTeng Ma</h1>
+         </div>
+      </section>
+   );
+};
